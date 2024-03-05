9f49dfc3e7380e5f7e72a1276fdc3d7b63ef7e24
Nabeel-x <121104595+Nabeel-x@users.noreply.github.com>
2024-03-05 13:59:00 +0530

    register form

--- project1/app1/views.py
+++ project1/app1/views.py
@@ -1,5 +1,24 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from .forms import *
+from .models import *
+from django.contrib import messages
+from django.contrib.auth import authenticate,login
+from django.conf import settings
+from django.core.mail import send_mail
 # Create your views here.
 def index(request):
     return render(request,'index.html')
+
+def register(request):
+    Rform = RegisterForm(request.POST)
+    dict={'Regform':Rform}
+    if request.method == "POST":
+        if Rform.is_valid():
+            user=Rform.save()
+            user.set_password(user.password)
+            user.save()
+            messages.success(request,"Registration successful..")
+            return redirect('login')
+        else:
+            messages.error(request,'Invalid form submission..')
+    return render(request,'Register.html',dict)
